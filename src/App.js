ceed9dbb40e75699f5da96f7de09b7276854d3fa
SiriFazendoBarra <manuel.martinez.perezdearce@gmail.com>
2022-12-11 19:44:39 -0300

    1st commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Formulario from './components/Formulario'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ejercicio formulario parte 1</h1>
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+function Formulario() {
+
+    const [nombre, setNombre] = useState("")
+    const [error, setError] = useState(false)
+
+    const validarInput = (e) => {
+        e.preventDefault()
+
+        if (nombre === "") {
+            setError(true)
+            return
+        }
+        setError(false)
+    }
+
+    return (
+        <form onSubmit={validarInput}>
+            {error ? <p className="error" >Debes ingresar tu nombre</p> : null}
+            <h3>{nombre}</h3>
+            <div className="form-group">
+                <input className="form-control" name="Nombre" onChange={(e) => { setNombre(e.target.value) }}></input>
+                <button className="btn btn-dark mt-3" type="submit">Enviar</button>
+            </div>
+        </form>
+    )
+}
+
+export default Formulario
